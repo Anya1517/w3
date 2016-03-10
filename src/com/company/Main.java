5b620be27c8cc93a44f7b24f3d2945041b88f589
kml_c_16 <Bilovus@kml.kh.ua>
2016-03-10 10:02:08 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //double a = 7;
+        //int b = 2;
+        //System.out.println(a/b);
+
+        //int a = 7;
+        //System.out.println(a++);
+        //System.out.println(++a);
+
+//        float x = 0, y = 0;
+//        x = (float)(0.3+0.4);
+//        y = 0.3f+0.4f;
+//        System.out.println((y));
+
+//        int a =7;
+//        double b = 0;
+//        double c = -0;
+//        double d = 2.5;
+//        double g = Double.NEGATIVE_INFINITY;
+//
+//        System.out.println(a/b);
+//        System.out.println(a/c);
+//        System.out.println(b == c);
+//        System.out.println(d/0);
+//        System.out.println(g*0);
+
+//        char c1 = '1';
+//        char c2 = '\u0031';
+//        char c3 = 49;
+//        System.out.println(c1);
+//        System.out.println(c2);
+//        System.out.println(c3);
+//        System.out.println(c1+c2+c3);
+
+//        for (char i = '\u0410'; i < '\u044F'; i++) {
+//            System.out.println(i);
+//        }
+
+        //System.out.println("4/2="+4/2=="4/2=2");
+
+        byte b1=50,b2=99;
+        byte k =(byte)(b1+b2);
+        System.out.println("k="+k);
+    }}
